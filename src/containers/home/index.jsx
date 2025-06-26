cc2193769c92dd4149391f82771341b1dce5f15a
ramanujm@yahoo.com <ramanujm@yahoo.com>
2025-06-26 14:14:57 -0400

    home page modified

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -14,31 +14,32 @@ const Home = () => {
 
     const link = document.createElement("a");
     link.href = fileUrl;
-    link.download = "RamanujM.pdf"; // rename the file if needed
+    link.download = "RamanujM.docx"; // rename the file if needed
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
 
   return (
-    <section id="home" className="home">
-      <div className="home__container_wrapper">
-        <h1>
-          Hello, I'm RAMANUJ
-          <br /> Turning ideas into impactful web solutions - A passionate
-          full-stack developer crafting innovative, scalable solutions that
-          bridge user needs with cutting-edge technology.
-        </h1>
-      </div>
-      <div className="home__container_buttons">
-        <div className="home__container_buttons__resume">
-          <button onClick={handleDownload}>Download</button>
+    <>
+      <section id="home" className="home">
+        <div className="home__container_wrapper">
+          <h1>Hello, I'm RAMANUJ</h1>
+          <div style={{ color: "#fff", fontSize: "21px", padding: "0px 20px" }}>
+            A passionate full-stack developer crafting innovative, scalable
+            solutions that bridge user needs with cutting-edge technology.
+          </div>
         </div>
-        <div className="home__container_buttons__contactMe">
-          <button onClick={handleHireMe}>Hire Me</button>
+        <div className="home__container_buttons">
+          <div className="home__container_buttons__resume">
+            <button onClick={handleDownload}>Download</button>
+          </div>
+          <div className="home__container_buttons__contactMe">
+            <button onClick={handleHireMe}>Hire Me</button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
